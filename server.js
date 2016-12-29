5b5327ec2d96b6528ed2e44d2ae1d79ecf1e49cf
Attaullah <awsumfriend7396@gmail.com>
2016-12-29 20:04:34 +0500

    fhahukfahkahjklfha

---
+++ server.js
@@ -0,0 +1,100 @@
+var path = require("path");
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var cookie = require("cookie-parser");
+var cors = require('cors');
+var app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(logger("dev"));
+app.use(cookie());
+
+console.log("kjfgqabhioaf");
+
+var sid = "ACf2d28794aa7e17939b16133113518409";
+var token = "3d8b88787cf7d13597718c83108d88c8";
+
+
+var authy = require('authy')("YBDqeRMkefWP7lSWkv0WR2ors33XJvJe");
+var twilioClient = require('twilio')(sid, token);
+
+
+var port = Number(process.env.PORT || 9000);
+/*var schema = require("./schema");*/
+
+
+app.get("*", function (req, res) {
+    res.sendFile(publicDirPath + "/index.html");
+});
+
+app.post('/sendSms', function (req, res) {
+    /*var accountSid = 'ACf2d28794aa7e17939b16133113518409';
+     var authToken = "your_auth_token";
+     var client = require('twilio')(accountSid, authToken);*/
+
+    /*twilioClient.outgoingCallerIds("PNe905d7e6b410746a0fb08c57e5a186f3").get(function(err, callerId) {
+     console.log(callerId.phoneNumber);
+     });*/
+
+
+    /* authy.register_user(req.body.email, req.body.phone, req.body.countryCode,
+     function(err, res) {
+     if(res){
+     console.log("user registered");
+     //var authId = res.user.id;
+     console.log(res);
+     console.log(res.user.id);
+     }else{
+     console.log("error");
+     console.log(err);
+     response.send("error");
+     }
+     });*/
+
+    console.log("req i");
+    console.log(req.body.phone);
+
+    twilioClient.sendMessage({
+        to: req.body.phone,
+        from: "+12052368776",
+        body: "Welcome to Notification App, Your verification code is " + req.body.code
+    }, function (err, response) {
+        if (err) {
+            console.log("error");
+            console.log(err);
+            res.send("error")
+        } else {
+            console.log("success");
+            res.send(response)
+        }
+    });
+
+});
+app.post('/recover', function (req, res) {
+    console.log("Recover chal para");
+
+    twilioClient.sendMessage({
+        to: req.body.phone,
+        from: "+12052368776",
+        body: "Hello from Notification App, Enter this code " + req.body.code + " to recover your account"
+    }, function (err, response) {
+        if (err) {
+            console.log("error");
+            console.log(err);
+            res.send("error")
+        } else {
+            console.log("success");
+            res.send(response)
+        }
+    });
+
+});
+
+
+app.listen(port, function () {
+    console.log("Server started on port 9000")
+});
